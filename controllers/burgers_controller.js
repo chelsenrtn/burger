f15992ba9e7dd3022f81afe05719ff1eea52c523
chelsenrtn <chelsea.nrtn@gmail.com>
2017-05-06 17:36:41 -0700

    Add files via upload

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+
+var models = require('../models');
+var burger = models.Burger;
+
+var sequelizeConnection = models.sequelize;
+
+module.exports = function(app) {
+        router.get('/', function(req, res) {
+            burger.findAll().then(function(burgers) {
+                res.render("index", {"burgers": burgers});
+            });
+        });
+        router.post("/", function(req, res) {
+            var r = req.body;
+            burger.create({
+                name: r.name,
+                devoured: false
+            }).then(function(burger){
+                res.redirect('/');
+            });
+        });
+        router.put('/devour', function(req, res) {
+            var r = req.body;
+            burger.findById(r.id).then(function(data){
+                data.update({
+                    devoured: true
+                }).then(function(){
+                    res.redirect('/');
+                })
+            })
+           
+        });
+        router.get('/deleteall', function(req, res){
+            burger.findAll().then(function(burgers){
+                burger.destroy({
+                    truncate: true
+                }).then(function(){
+                    res.redirect('/');
+                });
+            });
+        })
+
+        app.use('/', router);
+    }
---
+++ models/burgers.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define('Burger', {
+    name: DataTypes.STRING,
+    devoured: DataTypes.BOOLEAN
+  }, {
+    classMethods: {
+      associate: function(models) {
+      }
+    }
+  });
+  return Burger;
+};
